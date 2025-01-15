c83520678b2dc2807c18de0763c45ffe6bce9b0b
Atanas Atanasov <atanas.atanasov@ampeco.global>
2025-01-15 22:13:49 +0200

    add: ghostty
    add: fastfetch

---
+++ ghostty/setup.sh
@@ -0,0 +1,8 @@
+# Install
+brew install --cask ghostty
+
+# Add Fonts
+brew install --cask font-terminess-ttf-nerd-font
+
+# Link
+ln -s ./config $HOME/Library/Application\ Support/com.mitchellh.ghostty/config
