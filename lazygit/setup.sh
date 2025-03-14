ce2a691dfca585b910648795c0594c841ea62ae4
Atanas Atanasov <atanas.atanasov@ampeco.global>
2025-03-14 15:04:07 +0200

    add lazygit delta

--- lazygit/setup.sh
+++ lazygit/setup.sh
@@ -12,7 +12,7 @@ HOMEBREW_NO_AUTO_UPDATE=1 brew install lazygit
 echo "Linking..."
 
 src_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-dst_dir="${HOME}/Libgary/Application Support/lazygit"
+dst_dir="${HOME}/Library/Application\ Support/jesseduffield/lazygit"
 
 mkdir -p "${dst_dir}"
 ln -s "${src_dir}/config.yml" "${dst_dir}/config.yml"
