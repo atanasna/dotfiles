66c2307d188ce39f6b184fb0fdf6f8077e8825a1
atanasna <atanasna@atanasnas-MacBook-Pro.local>
2026-06-10 18:19:38 +0300

    fix: tmux tpm installation

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,12 +1,14 @@
 #---------------------
 # Install
 #---------------------
-# Tools 
-# HOMEBREW_NO_AUTO_UPDATE=1 brew install delta
-
 # Main 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# HOMEBREW_NO_AUTO_UPDATE=1 brew install lazygit
+HOMEBREW_NO_AUTO_UPDATE=1 brew install tmux
+
+mkdir -p "${HOME}/.tmux/plugins"
+if [[ ! -d "${HOME}/.tmux/plugins/tpm/.git" ]]; then
+  git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
+fi
+
 #---------------------
 # Link
 #---------------------
@@ -16,4 +18,7 @@ src_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 dst_dir="${HOME}"
 
 mkdir -p "${dst_dir}"
-ln -s "${src_dir}/tmux.conf" "${dst_dir}/.tmux.conf"
+ln -sfn "${src_dir}/tmux.conf" "${dst_dir}/.tmux.conf"
+
+# Install TPM plugins declared in tmux.conf, including Catppuccin.
+"${HOME}/.tmux/plugins/tpm/bin/install_plugins"
